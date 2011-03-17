7ed3ba7423837ff99f87c59609e9cc43ac5ddccf
Geoff Poore <g.poore@odey.com>
2011-03-17 18:18:06 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@360 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities.Enums/EventTypeIds.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+    [DataContract]
+    public enum EventTypeIds
+    {
+        [EnumMember]
+        None = 0,
+        [EnumMember]
+        TradeEvent = 1,
+        [EnumMember]
+        InstrumentEvent = 2,
+        [EnumMember]
+        CapitalEvent = 3
+    }
+}
