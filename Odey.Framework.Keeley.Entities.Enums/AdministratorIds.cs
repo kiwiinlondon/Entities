c106749531c5729d6d144d0dc83c3a765a26fdd9
Ross T <rosst@odey.com>
2012-10-10 10:09:31 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@2025 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities.Enums/AdministratorIds.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+    [DataContract]
+    public enum AdministratorIds
+    {
+        [DataMember]
+        RBCDexia = 4356,
+        [DataMember]
+        Capita =4514,
+        [DataMember]
+        Daiwa = 3845
+    }
+}
