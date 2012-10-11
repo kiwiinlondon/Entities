98cf23793dbbbe18dbb6ad88e3605cb4da3fea91
Ross T <rosst@odey.com>
2012-10-11 10:16:29 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@2043 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities.Enums/ClientSubTypeIds.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+    [DataContract]
+    public enum ClientSubTypeIds
+    {
+        [EnumMember]
+        Unkown = 1,
+        [EnumMember]
+        PrivateBankAssetManager = 3,
+        [EnumMember]
+        PensionFunds = 4,
+        [EnumMember]
+        PublicSector = 5,
+        [EnumMember]
+        Corporate = 6,
+        [EnumMember]
+        NonProfit = 7,
+        [EnumMember]
+        SubAdvisory = 8,
+        [EnumMember]
+        Insurance = 9,
+        [EnumMember]
+        Endowment = 10,
+        [EnumMember]
+        MultiManager = 11,
+        [EnumMember]
+        Charity = 12,
+        [EnumMember]
+        FundOfFunds = 13,
+        [EnumMember]
+        Staff = 14,
+        [EnumMember]
+        PrivateClient = 15,
+        [EnumMember]
+        IFA = 16,
+        [EnumMember]
+        FamilyOffice = 17,
+        [EnumMember]
+        FundOfFundsHedge = 18,
+        [EnumMember]
+        OdeyHoldingsAG = 19 
+    }
+}
---
+++ Odey.Framework.Keeley.Entities.Enums/ClientTradeTypeIds.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+    [DataContract]
+    public enum ClientTradeTypeIds
+    {
+        [EnumMember]
+        Normal = 1,
+        [EnumMember]
+        Transfer = 2
+    }
+}
