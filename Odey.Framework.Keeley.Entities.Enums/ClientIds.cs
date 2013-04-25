16e48a1c9ce639991e59961f8e4a196ea467a39e
Geoff Poore <g.poore@odey.com>
2013-04-25 08:55:45 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@2427 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities.Enums/ClientIds.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+    [DataContract]
+    public enum ClientIds
+    {
+        [EnumMember]
+        Dummy = 9706
+    }
+}
