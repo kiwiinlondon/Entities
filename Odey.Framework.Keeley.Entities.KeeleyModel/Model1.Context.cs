79d9222479590e3bcd73acc562b7d277ba9483e2
Alex Norman <alex.norman@odey.com>
2013-08-23 08:43:49 +0000

    index now indices
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@3134 a99c24c5-d062-0543-ab10-d6d61a48e989

--- Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
+++ Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
@@ -124,7 +124,7 @@ namespace Odey.Framework.Keeley.Entities
         public DbSet<FailedTrade> FailedTrades { get; set; }
         public DbSet<Beta> Beta { get; set; }
         public DbSet<FileToBeCollectedGroup> FileToBeCollectedGroup { get; set; }
-        public DbSet<Index> Indx { get; set; }
+        public DbSet<Index> Indices { get; set; }
     
         public virtual ObjectResult<PortfolioEvent> PortfolioEventGetPrevious(Nullable<int> positionID, Nullable<System.DateTime> referenceDate, Nullable<System.DateTime> inputDate, Nullable<int> orderingResolution, Nullable<int> portfolioAggregationLevelId, Nullable<int> portfolioEventId)
         {
