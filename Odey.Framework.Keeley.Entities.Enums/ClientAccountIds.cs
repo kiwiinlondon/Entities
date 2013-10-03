c86af26376512da9d5b02cb923d2d2243cb19d2e
Geoff Poore <g.poore@odey.com>
2013-10-03 10:12:56 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@3563 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities.Enums/ClientAccountIds.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+    public enum ClientAccountIds
+    {
+        OeiMacFeeder = 21384
+    }
+}
