5bf613d77a721de1e11c73f32006805a9633248d
Alex Norman <alex.norman@odey.com>
2013-10-04 10:49:08 +0000

    added ErrorEmailSendTime col
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@3580 a99c24c5-d062-0543-ab10-d6d61a48e989

--- Odey.Framework.Keeley.Entities/HealthCheck.cs
+++ Odey.Framework.Keeley.Entities/HealthCheck.cs
@@ -36,5 +36,7 @@ namespace Odey.Framework.Keeley.Entities
         public int UpdateUserID { get; set; }
     	[DataMember]
         public byte[] DataVersion { get; set; }
+    	[DataMember]
+        public Nullable<System.DateTime> ErrorEmailSendTime { get; set; }
     }
 }
