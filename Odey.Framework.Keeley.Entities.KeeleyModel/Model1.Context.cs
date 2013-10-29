47ff72da1aa31b2b26f71d13d7b3a55525d1b6ca
Geoff Poore <g.poore@odey.com>
2013-10-29 10:32:44 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@3871 a99c24c5-d062-0543-ab10-d6d61a48e989

--- Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
+++ Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
@@ -129,6 +129,7 @@ namespace Odey.Framework.Keeley.Entities
         public DbSet<ClientPortfolioBridge> ClientPortfolioBridges { get; set; }
         public DbSet<HealthCheck> HealthChecks { get; set; }
         public DbSet<FundCapitalChange> FundCapitalChanges { get; set; }
+        public DbSet<OfficialNetAssetValue> OfficialNetAssetValues { get; set; }
     
         public virtual ObjectResult<PortfolioEvent> PortfolioEventGetPrevious(Nullable<int> positionID, Nullable<System.DateTime> referenceDate, Nullable<System.DateTime> inputDate, Nullable<int> orderingResolution, Nullable<int> portfolioAggregationLevelId, Nullable<int> portfolioEventId)
         {
