a2a8103a7650366c875ddc1cb65a15fb59242e42
Geoff Poore <g.poore@odey.com>
2013-11-20 11:11:32 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@4002 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities/Partial Classes/ClientAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Odey.Framework.Keeley.Entities
+{
+    partial class ClientAccount    
+    {
+        public override string ToString()
+        {
+            return String.Format("Client Account: ClientAccountId = {0}, ClientId={1}, AdministratorId={2}, FundId={3}, Name={4}", this.ClientAccountId, this.ClientId, this.AdministratorId, this.FundId, this.Name);
+        }
+    }
+}
