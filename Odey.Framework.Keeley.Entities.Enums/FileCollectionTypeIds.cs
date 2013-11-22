da62d419dcd9fea3287ad98e5f35cd26e23141fe
Geoff Poore <g.poore@odey.com>
2013-11-22 17:01:16 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@4051 a99c24c5-d062-0543-ab10-d6d61a48e989

--- Odey.Framework.Keeley.Entities.Enums/FileCollectionTypeIds.cs
+++ Odey.Framework.Keeley.Entities.Enums/FileCollectionTypeIds.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Odey.Framework.Keeley.Entities.Enums
 {
+    [DataContract]
     public enum FileCollectionTypeIds
     {
-        SFTP=1,
-        FTP=2
+        [EnumMember]
+        SFTP = 1,
+        [EnumMember]
+        FTP = 2,
+        [EnumMember]
+        Directory = 3
     }
 }
