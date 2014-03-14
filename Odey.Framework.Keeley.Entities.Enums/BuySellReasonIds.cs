60e3cdd95f6e968504ac8b61d90bae496bc97ac1
Geoff Poore <g.poore@odey.com>
2014-03-14 17:24:21 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@4916 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities.Enums/BuySellReasonIds.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+    [DataContract]
+    public enum BuySellReasonIds
+    {
+        [EnumMember]
+        Buy = 1,
+        [EnumMember]
+        Sell = 2,
+        [EnumMember]
+        BuyCover = 3,
+        [EnumMember]
+        SellToOpen = 4,
+        [EnumMember]
+        BuyToClose = 5,
+        [EnumMember]
+        SellShort = 6,
+        [EnumMember]
+        BuyToOpen = 7,
+        [EnumMember]
+        SellToClose = 8,
+        [EnumMember]
+        Redemption = 9,
+        [EnumMember]
+        Subscription = 10,
+        [EnumMember]
+        SubscriptionSwitch = 11
+    }
+}
