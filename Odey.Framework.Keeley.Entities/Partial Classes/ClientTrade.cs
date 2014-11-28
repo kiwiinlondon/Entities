5e85ba5a2d8278a2224cbbc854ba7e73d0414a93
Geoff Poore <g.poore@odey.com>
2014-11-28 15:25:22 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@6464 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities/Partial Classes/ClientTrade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Odey.Framework.Keeley.Entities
+{
+    partial class ClientTrade
+    {
+         public override string ToString()
+        {
+            return String.Format("Client Trade: ClientAccountId = {0}, TradeReference={1}, TradeDate={2}, Quantity={3}, Price={4}",
+                this.ClientAccountId, this.TradeReference,this.TradeDate,this.Quantity,this.Price);
+        }
+    }
+}
