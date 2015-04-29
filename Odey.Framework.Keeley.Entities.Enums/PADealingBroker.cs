c77cbc6e74cf606bf9f8d132c7e1bb31e0172049
Carlos Ruiz <c.ruiz@odey.com>
2015-04-29 14:20:47 +0000

    PADealingBroker Enum added with Odey Wealth Management only
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@7704 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities.Enums/PADealingBroker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+    [DataContract]
+
+    public enum PADealingBroker
+    {
+        [Description("Odey Wealth Management (UK) Ltd")]
+        [EnumMember]
+        OdeyWealthManagementUK = 6783,
+       
+    }
+}
