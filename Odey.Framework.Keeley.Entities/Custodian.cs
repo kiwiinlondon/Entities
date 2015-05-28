9dfb25fbdd4fa4629c022d2671b47ffaa8951e70
Carlos Ruiz <c.ruiz@odey.com>
2015-05-28 15:02:09 +0000

    made Custodian.LegalEntity public member
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@7980 a99c24c5-d062-0543-ab10-d6d61a48e989

--- Odey.Framework.Keeley.Entities/Custodian.cs
+++ Odey.Framework.Keeley.Entities/Custodian.cs
@@ -26,6 +26,6 @@ namespace Odey.Framework.Keeley.Entities
         public byte[] DataVersion { get; set; }
     
         [DataMember]
-        private LegalEntity LegalEntity { get; set; }
+        public LegalEntity LegalEntity { get; set; }
     }
 }
