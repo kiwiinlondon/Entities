3f05326ba44de3eeb509092d5ae748728c9cfdcb
Geoff Poore <g.poore@odey.com>
2015-05-29 16:21:50 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@8004 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities/Partial Classes/EntityType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Odey.Framework.Keeley.Entities
+{
+    partial class EntityType   
+    {
+        private EntityProperty _primarykey;
+
+        public EntityProperty PrimaryKey
+        {
+            get
+            {
+                if (_primarykey == null)
+                {
+                    _primarykey = EntityProperties.FirstOrDefault(a => a.IsPrimaryKey);
+                }
+                return _primarykey;
+            }
+        }
+
+        Dictionary<int,EntityProperty> _entityPropertyHashed;
+        public EntityProperty GetEntityProperty(int entityPropertyId)
+        {
+            if (_entityPropertyHashed == null)
+            {
+                _entityPropertyHashed = EntityProperties.ToDictionary(a => a.EntityPropertyID, a => a);
+            }
+            return _entityPropertyHashed[entityPropertyId];
+        }
+    }
+}
