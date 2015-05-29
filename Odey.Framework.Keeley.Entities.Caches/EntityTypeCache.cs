97d31a39fd4dbbcbccd26d330535e5409b07caf6
Geoff Poore <g.poore@odey.com>
2015-05-29 16:20:59 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@8000 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities.Caches/EntityTypeCache.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace Odey.Framework.Keeley.Entities.Caches
+{
+
+    public class EntityTypeCache : KeeleyCache<string, EntityType>
+    {
+        protected override Dictionary<string, EntityType> CreateCache(KeeleyModel context)
+        {
+            return context.EntityTypes.Include(a=>a.EntityProperties).ToDictionary(a => a.FullyQualifiedName, a => a);
+        }
+
+
+        protected override KeyNotFoundBehaviourIds KeyNotFoundBehaviour
+        {
+            get { return KeyNotFoundBehaviourIds.All; }
+        }
+    }
+}
