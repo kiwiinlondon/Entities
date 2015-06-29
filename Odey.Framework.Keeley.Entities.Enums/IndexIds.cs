ce590cf287d4d27b0a61c749e2302c04d17ed68a
Alex Norman <alex.norman@odey.com>
2015-06-29 13:40:32 +0000

    more index enum
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@8262 a99c24c5-d062-0543-ab10-d6d61a48e989

--- Odey.Framework.Keeley.Entities.Enums/IndexIds.cs
+++ Odey.Framework.Keeley.Entities.Enums/IndexIds.cs
@@ -9,9 +9,39 @@ namespace Odey.Framework.Keeley.Entities.Enums
     [DataContract]
     public enum IndexIds
     {    
+        
         [EnumMember]
-        MSCIWorldNetTR = 9920,
+        HFRX_Macro_Multi_Strategy = 20442,
+        
         [EnumMember]
-        HFRXMacroMultiStrategy = 20442
+        BAIF = 11668,
+        
+        [EnumMember]
+        FT_WLD_EUROPE_TRI_EUR = 22666,
+        
+        [EnumMember]
+        FTSE_WMA_Balanced = 9922,
+        
+        [EnumMember]
+        MSCI_Daily_TR_Gross_Europe = 9918,
+        
+        [EnumMember]
+        MSCI_Daily_TR_Gross_World_USD = 18763,
+        
+        [EnumMember]
+        MSCI_Daily_TR_Net_Europe = 9916,
+        
+        [EnumMember]
+        MSCI_Daily_TR_Net_Europe_Ex_UK = 9917,
+        
+        [EnumMember]
+        MSCI_World_Net_TR_World = 9920,
+        
+        [EnumMember]
+        MSCI_Europe_Index = 9915,
+
+        [EnumMember]
+        MSCI_World_Index = 9919,
+
     }
 }
