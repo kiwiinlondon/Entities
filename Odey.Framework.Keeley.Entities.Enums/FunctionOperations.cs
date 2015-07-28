e0d9272cf3871be2b3d4f4883fc03c2ae9377046
Carlos Ruiz <c.ruiz@odey.com>
2015-07-28 13:35:51 +0000

    Function Operations Flag
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@8640 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities.Enums/FunctionOperations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+    [Flags]
+    public enum FunctionOperations
+    {
+        None =        0,
+        Create = 1 << 0,
+        Read =   1 << 1,
+        Update = 1 << 2,
+        Delete = 1 << 3,
+    }
+}
