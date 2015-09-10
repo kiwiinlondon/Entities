1e6e19a852830ba902fa7cd167bcd85ff3f4426c
Geoff Poore <g.poore@odey.com>
2015-09-10 14:57:56 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@9331 a99c24c5-d062-0543-ab10-d6d61a48e989

--- Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
+++ Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
@@ -146,7 +146,6 @@ namespace Odey.Framework.Keeley.Entities
         public DbSet<SecurityGroup> SecurityGroups { get; set; }
         public DbSet<SecurityGroupFunctionPoint> SecurityGroupFunctionPoints { get; set; }
         public DbSet<AdministratorPortfolio> AdministratorPortfolios { get; set; }
-        public DbSet<AdministratorOpeningNAV> AdministratorOpeningNAVs { get; set; }
     
         public virtual ObjectResult<PortfolioEvent> PortfolioEventGetPrevious(Nullable<int> positionID, Nullable<System.DateTime> referenceDate, Nullable<System.DateTime> inputDate, Nullable<int> orderingResolution, Nullable<int> portfolioAggregationLevelId, Nullable<int> portfolioEventId)
         {
