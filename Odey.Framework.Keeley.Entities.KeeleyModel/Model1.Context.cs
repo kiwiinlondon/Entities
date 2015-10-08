69287400eaaa46267711442cf677f63b0ae84419
Geoff Poore <g.poore@odey.com>
2015-10-08 10:17:44 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@9700 a99c24c5-d062-0543-ab10-d6d61a48e989

--- Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
+++ Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
@@ -148,6 +148,7 @@ namespace Odey.Framework.Keeley.Entities
         public DbSet<FundCountryHoliday> FundCountryHolidays { get; set; }
         public DbSet<CorporateAction> CorporateActions { get; set; }
         public DbSet<CorporateActionType> CorporateActionTypes { get; set; }
+        public DbSet<Attribution> Attributions { get; set; }
     
         public virtual ObjectResult<PortfolioEvent> PortfolioEventGetPrevious(Nullable<int> positionID, Nullable<System.DateTime> referenceDate, Nullable<System.DateTime> inputDate, Nullable<int> orderingResolution, Nullable<int> portfolioAggregationLevelId, Nullable<int> portfolioEventId)
         {
