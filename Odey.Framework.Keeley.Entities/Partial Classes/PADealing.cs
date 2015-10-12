3430c36651069d5b957650cb07b8e7b4580dc970
Carlos Ruiz <c.ruiz@odey.com>
2015-10-12 16:39:03 +0000

    Added ToString to PADealing class
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@9733 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities/Partial Classes/PADealing.cs
@@ -0,0 +1,12 @@
+﻿namespace Odey.Framework.Keeley.Entities
+{
+    public partial class PADealing
+    {
+        public override string ToString()
+        {
+            return $"PADealingID:{PADealingID} - RequestUserId:{RequestUserID} - InstrumentMarketId:{InstrumentMarketID} - AccountId:{PADealingAccountID} - RequestQuantity:{RequestQuantity} - RequestValue:{RequestValue}" +
+                   $"IsExistingHolding:{IsExistingHolding} - ActualQuantity:{ActualQuantity} - ActualValue:{ActualValue} - Cost:{Cost}" +
+                   $"IsComplianceApproved:{IsComplianceApproved} - ComplianceUserID:{ComplianceUserID} - IsTraderApproved:{IsTraderApproved} - TraderUserID:{TraderUserID}";
+        }
+    }
+}
---
+++ Odey.Framework.Keeley.Entities/Partial Classes/PADealingBalance.cs
@@ -0,0 +1,10 @@
+﻿namespace Odey.Framework.Keeley.Entities
+{
+    public partial class PADealingBalance
+    {
+        public override string ToString()
+        {
+            return $"PADealingBalanceID:{PADealingBalanceID} - UserId:{UserID} - InstrumentMarketId:{InstrumentMarketID} - UserAccountId:{UserAccountID} - Quantity:{Quantity} - BookCost:{BookCost} - RealisedPNL:{RealisedPNL}";
+        }
+    }
+}
