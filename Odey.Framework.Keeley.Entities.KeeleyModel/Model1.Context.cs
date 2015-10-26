ef428a60a0756fd354047c71ddfe9e72a45b692c
Geoff Poore <g.poore@odey.com>
2015-10-26 17:19:51 +0000

    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@9929 a99c24c5-d062-0543-ab10-d6d61a48e989

--- Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
+++ Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
@@ -152,6 +152,7 @@ namespace Odey.Framework.Keeley.Entities
         public DbSet<RiskAnalytic> RiskAnalytics { get; set; }
         public DbSet<RiskAnalyticType> RiskAnalyticTypes { get; set; }
         public DbSet<BookNetAssetValue> BookNetAssetValues { get; set; }
+        public DbSet<AttributionSource> AttributionSources { get; set; }
     
         public virtual ObjectResult<PortfolioEvent> PortfolioEventGetPrevious(Nullable<int> positionID, Nullable<System.DateTime> referenceDate, Nullable<System.DateTime> inputDate, Nullable<int> orderingResolution, Nullable<int> portfolioAggregationLevelId, Nullable<int> portfolioEventId)
         {
