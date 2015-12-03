938f929007c50706b7f3fd2bdd0ba62d7972afd9
Alex Norman <alex.norman@odey.com>
2015-12-03 15:54:41 +0000

    added RiskAnalyticTypeIds
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@10397 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities.Enums/RiskAnalyticTypeIds.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Runtime.Serialization;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+ [DataContract]
+    public enum RiskAnalyticTypeIds
+    {
+
+        [EnumMember]
+        TotalRisk = 1,
+        
+        [EnumMember]
+        PercentSystematicTotalRisk = 2,
+
+        [EnumMember]
+        Sep2001September11th = 28,
+
+        [EnumMember]
+        IndexVaRPrimarySettings = 31,
+
+        [EnumMember]
+        IndexVaRSecondarySettings = 33,
+
+
+    }
+}
+
