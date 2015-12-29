5765c6116c34498af8173243bb87530f817f7c70
Alex Norman <alex.norman@odey.com>
2015-12-29 15:33:57 +0000

    added issuer analytic partial classes too
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@10639 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities/Partial Classes/IssuerAnalytic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Odey.Framework.Keeley.Entities.Interfaces;
+using Odey.Framework.Keeley.Entities.Enums;
+
+namespace Odey.Framework.Keeley.Entities
+{
+    partial class IssuerAnalytic : IMarketDatum
+    {
+        #region IMarketDatum Members
+
+        public int EntityRankingSchemeId
+        {
+            get { return (int)EntityRankingSchemeIds.Default; }
+        }
+
+        public List<IRawMarketDatum> RawMarketData
+        {
+
+            get
+            {
+                if (RawIssuerAnalytic != null)
+                {
+                    return new List<IRawMarketDatum> { RawIssuerAnalytic };
+                }
+                else
+                {
+                    return new List<IRawMarketDatum>();
+                }
+            }
+        
+            
+        }
+
+        #endregion
+    }
+ 
+}
---
+++ Odey.Framework.Keeley.Entities/Partial Classes/RawIssuerAnalytic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Odey.Framework.Keeley.Entities.Interfaces;
+
+namespace Odey.Framework.Keeley.Entities
+{
+    partial class RawIssuerAnalytic : IRawMarketDatum
+    {
+        int IRawMarketDatum.RawMarketDatumId
+        {
+            get
+            {
+                return RawIssuerAnalyticId;
+            }
+        }
+
+
+
+
+        List<IMarketDatum> IRawMarketDatum.MarketData
+        {
+            get { return IssuerAnalytics.ToList<IMarketDatum>(); }
+        }
+    }
+}
