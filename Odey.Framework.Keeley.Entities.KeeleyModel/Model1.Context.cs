c2bcb0c511bc36a887cbe3c3b2251bc88763f59f
Alex Norman <alex.norman@odey.com>
2015-12-29 10:50:01 +0000

    issuer Analytic
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@10626 a99c24c5-d062-0543-ab10-d6d61a48e989

--- Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
+++ Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
@@ -153,6 +153,8 @@ namespace Odey.Framework.Keeley.Entities
         public DbSet<BookNetAssetValue> BookNetAssetValues { get; set; }
         public DbSet<AttributionSource> AttributionSources { get; set; }
         public DbSet<OpenedClosedPosition> OpenedClosedPositions { get; set; }
+        public DbSet<IssuerAnalytic> IssuerAnalytics { get; set; }
+        public DbSet<RawIssuerAnalytic> RawIssuerAnalytics { get; set; }
     
         public virtual ObjectResult<PortfolioEvent> PortfolioEventGetPrevious(Nullable<int> positionID, Nullable<System.DateTime> referenceDate, Nullable<System.DateTime> inputDate, Nullable<int> orderingResolution, Nullable<int> portfolioAggregationLevelId, Nullable<int> portfolioEventId)
         {
