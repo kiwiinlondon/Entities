3688e05d66239c03743b7322c48a90d882885da1
Carlos Ruiz <c.ruiz@odey.com>
2016-01-19 17:47:07 +0000

    Client Trailsz
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@10894 a99c24c5-d062-0543-ab10-d6d61a48e989

--- Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
+++ Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
@@ -155,6 +155,7 @@ namespace Odey.Framework.Keeley.Entities
         public DbSet<RawIssuerAnalytic> RawIssuerAnalytics { get; set; }
         public DbSet<AttributionFund> AttributionFunds { get; set; }
         public DbSet<Attribution> Attributions { get; set; }
+        public DbSet<ClientTrail> ClientTrails { get; set; }
     
         public virtual ObjectResult<PortfolioEvent> PortfolioEventGetPrevious(Nullable<int> positionID, Nullable<System.DateTime> referenceDate, Nullable<System.DateTime> inputDate, Nullable<int> orderingResolution, Nullable<int> portfolioAggregationLevelId, Nullable<int> portfolioEventId)
         {
