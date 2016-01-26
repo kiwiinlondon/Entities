2447f280d7317e2df8d20fec9020c0ecfa149839
Carlos Ruiz <c.ruiz@odey.com>
2016-01-26 17:50:03 +0000

    Added to Administrator Name/Long Name
    git-svn-id: https://odey-ops01.oam.odey.com/svn/odey/Odey.Framework.Keeley.Entities@11040 a99c24c5-d062-0543-ab10-d6d61a48e989

---
+++ Odey.Framework.Keeley.Entities/Partial Classes/Administrator.cs
@@ -0,0 +1,27 @@
+﻿namespace Odey.Framework.Keeley.Entities
+{
+    partial class Administrator
+    {
+        public Administrator(LegalEntity legalEntity)
+            : this()
+        {
+            LegalEntity = legalEntity;
+        }
+        
+        public string Name
+        {
+            get
+            {
+                return LegalEntity.Name;
+            }
+        }
+
+        public string LongName
+        {
+            get
+            {
+                return LegalEntity.LongName;
+            }
+        }
+    }
+}
