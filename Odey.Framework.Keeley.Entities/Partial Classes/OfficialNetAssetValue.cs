c8bf23cbb78b5f8a392dfb1c994a40aa67aa292c
geoffp <g.poore@odey.com>
2016-09-01 14:54:08 +0100

    added clone to official nav

---
+++ Odey.Framework.Keeley.Entities/Partial Classes/OfficialNetAssetValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Odey.Framework.Keeley.Entities
+{
+    partial class OfficialNetAssetValue
+    {
+        public OfficialNetAssetValue ShallowCopy()
+        {
+            return (OfficialNetAssetValue)this.MemberwiseClone();
+        }
+    }
+}
