17ab9c71b8cd1801d2ea31f93ec228e0726876c9
geoffp <g.poore@odey.com>
2017-01-24 15:01:56 +0000

    fixed

--- Odey.Framework.Keeley.Entities.Enums/CompanySizeIds.cs
+++ Odey.Framework.Keeley.Entities.Enums/CompanySizeIds.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -10,6 +11,33 @@ namespace Odey.Framework.Keeley.Entities.Enums
     public enum CompanySizeIds
     {
         [EnumMember]
-        Unknown = 1
+        [Description("Unknown")]
+        Unknown = 1,
+
+
+        [Description("Nano")]
+        [EnumMember]
+        Nano = 2,
+
+        [Description("Micro")]
+        [EnumMember]
+        Micro = 3,
+
+        [Description("Small")]
+        [EnumMember]
+        Small = 4,
+
+        [Description("Mid")]
+        [EnumMember]
+        Mid = 5,
+
+        [Description("Big")]
+        [EnumMember]
+        Big = 6,
+
+        [Description("Mega")]
+        [EnumMember]
+        Mega =7
+
     }
 }
