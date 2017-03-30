6070df6f6d3a77e68e427f1515f8334b1ba3b4f1
geoffp <g.poore@odey.com>
2017-03-30 15:39:51 +0100

    administrator mv on portfolio

--- Odey.Framework.Keeley.Entities/Portfolio.cs
+++ Odey.Framework.Keeley.Entities/Portfolio.cs
@@ -210,6 +210,8 @@ namespace Odey.Framework.Keeley.Entities
         public Nullable<decimal> ValuationTomorrowDeltaNetCostInstrumentCurrency { get; set; }
     	[DataMember]
         public Nullable<decimal> AdministratorPrice { get; set; }
+    	[DataMember]
+        public Nullable<decimal> AdministratorMarketValue { get; set; }
     
         [DataMember]
         public virtual Position Position { get; set; }
