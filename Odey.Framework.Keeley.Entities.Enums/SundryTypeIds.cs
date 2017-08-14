e352a60a2a53888008a7e6185d71abdab636a0d8
geoffp <g.poore@odey.com>
2017-08-14 14:56:06 +0100

    attribution sundry

---
+++ Odey.Framework.Keeley.Entities.Enums/SundryTypeIds.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+    [DataContract]
+    public enum SundryTypeIds
+    {
+        [EnumMember]
+        ManagementFee = 1,
+        [EnumMember]
+        PerformanceFee = 2,
+        [EnumMember]
+        OtherGeneralLedger = 3,
+        [EnumMember]
+        UnmatchedInstrument = 4,
+    }
+}
