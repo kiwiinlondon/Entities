31e7279588833b1627356c229bef17fe1730a3e9
bradp <b.parker@odey.com>
2017-09-11 11:26:21 +0100

    ClientMarketingTypeIds

---
+++ Odey.Framework.Keeley.Entities.Enums/ClientMarketingTypeIds.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+    [DataContract]
+    public enum ClientMarketingTypeIds
+    {
+        [EnumMember]
+        Retail = 1,
+        [EnumMember]
+        Institutional = 2,
+        [EnumMember]
+        Management = 3,
+        [EnumMember]
+        InstitutionalOWMClientsOnly = 4,
+        [EnumMember]
+        RetailOWMClientsOnly = 5,
+    }
+}
