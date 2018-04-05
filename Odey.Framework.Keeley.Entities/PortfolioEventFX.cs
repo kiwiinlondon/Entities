d22d02697a2e3419fc684232670c82cdf7b81c0d
geoffp <g.poore@odey.com>
2018-04-05 11:13:29 +0100

    fxrate on portfolioevent fx

--- Odey.Framework.Keeley.Entities/PortfolioEventFX.cs
+++ Odey.Framework.Keeley.Entities/PortfolioEventFX.cs
@@ -40,5 +40,7 @@ namespace Odey.Framework.Keeley.Entities
         public int UpdateUserID { get; set; }
     	[DataMember]
         public byte[] DataVersion { get; set; }
+    	[DataMember]
+        public Nullable<int> FXRateId { get; set; }
     }
 }
