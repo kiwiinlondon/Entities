31d62a34dd963ae4c70b4d67b6a5bad093fbdbd4
geoffp <g.poore@odey.com>
2018-11-15 16:54:32 +0000

    eze trades

---
+++ Odey.Framework.Keeley.Entities/Interfaces/IEzeTrade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Odey.Framework.Keeley.Entities.Interfaces
+{
+    public interface IEzeTrade
+    {
+        string ParentTradeID { get; set; }
+        string TradeID { get; set; }
+        string LotID { get; set; }
+        string FMID { get; set; }
+        string BrokerCode { get; set; }
+
+        Nullable<System.DateTime> OrderTimestamp { get; set; }
+
+        Nullable<System.DateTime> TradeTimestamp { get; set; }
+
+        decimal Quantity { get; set; }
+
+        decimal ResearchCommission { get; set; }
+        Nullable<double> ExecutionCommission { get; set; }
+
+        Nullable<double> CFD_GrossPrice { get; set; }
+
+        Nullable<double> CFD_GrossMoney { get; set; }
+        Nullable<double> CFD_ResearchCommission { get; set; }
+
+        int CFDFlag { get; set; }
+
+        int CashFlag { get; set; }
+
+        int ImpliedCommsFlag { get; set; }
+
+        string TradeEntrySource { get; set; }
+    }
+}
