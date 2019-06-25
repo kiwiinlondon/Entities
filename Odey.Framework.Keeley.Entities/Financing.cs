774aed3be3ba63701d4ac5386e43fa3cbf62cf9f
geoffp <g.poore@odey.com>
2019-06-25 15:58:35 +0100

    over borrow

--- Odey.Framework.Keeley.Entities/Financing.cs
+++ Odey.Framework.Keeley.Entities/Financing.cs
@@ -52,6 +52,8 @@ namespace Odey.Framework.Keeley.Entities
         public int UpdateUserID { get; set; }
     	[DataMember]
         public byte[] DataVersion { get; set; }
+    	[DataMember]
+        public Nullable<bool> IsOverBorrow { get; set; }
     
         [DataMember]
         public virtual Currency Currency { get; set; }
