88038f967a8c42abe2a27eea6b70ac173ecd0025
Geoff Poore <g.poore@odey.com>
2021-07-05 11:48:26 +0100

    b

--- Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
+++ Odey.Framework.Keeley.Entities.KeeleyModel/Model1.Context.cs
@@ -209,6 +209,7 @@ namespace Odey.Framework.Keeley.Entities
         public DbSet<LoanedPortfolioCollatoral> LoanedPortfolioCollatorals { get; set; }
         public DbSet<MessageQueue> MessageQueues { get; set; }
         public DbSet<RepoTradeEvent> RepoTradeEvents { get; set; }
+        public DbSet<Repo> Repoes { get; set; }
     
         public virtual ObjectResult<PortfolioEvent> PortfolioEventGetPrevious(Nullable<int> positionID, Nullable<System.DateTime> referenceDate, Nullable<System.DateTime> inputDate, Nullable<int> orderingResolution, Nullable<int> portfolioAggregationLevelId, Nullable<int> portfolioEventId)
         {
