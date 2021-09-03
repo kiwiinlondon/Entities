0b957f3ecaa13825ed98741fa7165960e1d957c4
Geoff Poore <g.poore@odey.com>
2021-09-03 13:23:59 +0100

    s

---
+++ Odey.Framework.Keeley.Entities.Enums/PerformanceFeeTypeIds.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odey.Framework.Keeley.Entities.Enums
+{
+     [DataContract]
+    public enum PerformanceFeeTypeIds
+    {
+        [EnumMember]
+        Absolute=1,
+        [EnumMember]
+        Relative=2
+    }
+}
